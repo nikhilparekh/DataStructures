d51184b1aff8422dd505de4355692f38477bbf89
Nikhil Parekh <nksp777@gmail.com>
2020-08-16 16:24:27 -0700

    Added Pre-Order Traversal

--- BinaryTree.py
+++ BinaryTree.py
@@ -6,13 +6,31 @@ class Node(object):
         
 
 class BinaryTree(object):
-    def __init(self,root):
+    def __init__(self,root):
         self.root = Node(root)
+    
+    def printTree(self,Traversal_type):
+        if(Traversal_type=="preorder"):
+            return self.preorder(tree.root,"")
+        
+
+    def preorder(self,start,traversal):
+        if start:
+            traversal+=(str(start.data) + " - ")
+            traversal = self.preorder(start.left,traversal)
+            traversal = self.preorder(start.right, traversal)
+        return traversal
 
 #setting up tree
-
-tree = BinaryTree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
+tree = BinaryTree("F")
+tree.root.left = Node("B")
+tree.root.right = Node("G")
+tree.root.left.left = Node("A")
+tree.root.left.right = Node("D")
+tree.root.left.right.left = Node("C")
+tree.root.left.right.right = Node("E")
+tree.root.right.right = Node("I")
+tree.root.right.right.left = Node("H")
     
+print(tree.printTree("preorder"))
 
