e0aeacbe307ead45f113791ca13fdf5ac7e20ec2
Nikhil Parekh <nksp777@gmail.com>
2020-08-29 19:40:33 -0700

    Selection sort Added

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,9 +1,9 @@
 a = [2,3,5,6,8,10,15,1,7]
 
-for i in range(len(a)-1):
-    temp = a[i]
-    for j in range(i,len(a)):
-        if(a[j]<temp):
-            temp = a[j]
-    a[i],temp = temp,a[i]
+for i in range(len(a)):
+    temp = i
+    for j in range(i+1,len(a)):
+        if(a[j]<a[temp]):
+            temp = j
+    a[i],a[temp] = a[temp],a[i]
     print(a)
